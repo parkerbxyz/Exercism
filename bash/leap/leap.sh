5780d8a6854241e26398e6edaa2e227b8de34d46
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-04-07 21:36:59 -0400

    Solve leap exercise

---
+++ bash/leap/leap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+year=$1
+
+if ! [[ $# -eq 1 ]] || ! [[ $year =~ ^[0-9]{4}$ ]] ; then
+  { echo "Usage: $0 <year>"; exit 1; }
+fi
+
+if [[ $(($year % 4)) -eq 0 ]] \
+  && [[ $(($year % 100)) -ne 0 ]] \
+  || [[ $(($year % 400)) -eq 0 ]]; then
+  echo 'true'
+else
+  echo 'false'
+fi
