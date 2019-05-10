b946d6e187d1c7c0b614a38d625a3ee1d12058bc
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-05-09 21:45:51 -0400

    Solve bob.py

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,11 @@
-def hey(phrase):
-    pass
+def hey(phrase: str) -> str:
+    """Return Bob's response to a given phrase."""
+    phrase = phrase.strip()
+    if phrase.endswith('?'):
+        return ("Calm down, I know what I'm doing!"
+                if phrase.isupper() else "Sure.")
+    if phrase.isupper():
+        return "Whoa, chill out!"
+    if len(phrase) == 0:
+        return "Fine. Be that way!"
+    return "Whatever."
