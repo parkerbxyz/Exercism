1ab6b1e5ed14577fe1be8fdc10b4373564679214
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-05-10 16:10:21 -0400

    Refactor bob.py

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,11 +1,12 @@
 def hey(phrase: str) -> str:
     """Return Bob's response to a given phrase."""
     phrase = phrase.strip()
-    if phrase.endswith('?'):
-        return ("Calm down, I know what I'm doing!"
-                if phrase.isupper() else "Sure.")
-    if phrase.isupper():
-        return "Whoa, chill out!"
-    if not phrase:
+    if not phrase:  # if you address him without actually saying anything
         return "Fine. Be that way!"
-    return "Whatever."
+    if phrase.isupper():  # if you yell at him
+        if phrase.endswith('?'):   # if you yell a question at him
+            return "Calm down, I know what I'm doing!"
+        return "Whoa, chill out!"
+    if phrase.endswith('?'):  # if you ask him a question
+        return "Sure."
+    return "Whatever."  # to anything else
