f3f72e8c3d122cd8a5319bece2e7e174c61717aa
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-05-09 23:26:50 -0400

    Solve grains.py

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,20 @@
-def on_square(integer_number):
-    pass
+from functools import wraps
 
 
-def total_after(integer_number):
-    pass
+def validate_range(func):
+    @wraps(func)
+    def wrapper(number: int) -> int:
+        if number > 64 or number < 1:
+            raise ValueError("Invalid input. Must be in the range 1-64.")
+        return func(number)
+    return wrapper
+
+
+@validate_range
+def on_square(number: int) -> int:
+    return (2 ** number - 1) - (2 ** (number - 1) - 1)
+
+
+@validate_range
+def total_after(number: int) -> int:
+    return 2 ** number - 1
