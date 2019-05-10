98bb31eadb154cc50147eeaad7e7f0778d7cdb22
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-05-09 21:06:34 -0400

    Add docstring to is_isogram function

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,3 +1,4 @@
 def is_isogram(string: str) -> bool:
+    """Return True if a given word or phrase is an isogram."""
     letters = [letter.casefold() for letter in string if letter.isalpha()]
     return len(letters) == len(set(letters))
