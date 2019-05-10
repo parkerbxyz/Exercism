77fa0e471aa16301ce3610305a19f0d30174dd6a
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-05-10 15:43:36 -0400

    Refactor darts.py
    - Reordered if statements
    - Changed some if statements to elif
    - Added an else statement

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -7,10 +7,11 @@ Point = Union[int, float]
 def score(x: Point, y: Point) -> int:
     """Returns the earned points in a single toss of a Darts game."""
     distance = sqrt(x**2 + y**2)
-    if distance > 10:  # outside the target
-        return 0
-    if distance > 5:  # outer circle of the target
-        return 1
-    if distance > 1:  # middle circle of the target
+    if distance <= 1:  # inner circle of the target
+        return 10
+    elif distance <= 5:  # middle circle of the target
         return 5
-    return 10  # inner circle of the target
+    elif distance <= 10:  # outer circle of the target
+        return 1
+    else:
+        return 0  # outside the target
