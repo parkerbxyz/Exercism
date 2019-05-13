6fdf117c1e5bf3d9711f760935d060cf7c8c2f98
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-05-13 15:02:35 -0400

    Update gigasecond.rb

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,7 +1,8 @@
+# Calculate the moment when someone has lived for 10^9 seconds.
 class Gigasecond
-  GIGASECOND = 10**9  # 1,000,000,000 seconds
+  SECONDS = 10**9 # 1,000,000,000
 
-  def self.from(time)
-    time + GIGASECOND
+  def self.from(birthdate)
+    birthdate + SECONDS
   end
 end
