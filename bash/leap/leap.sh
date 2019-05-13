89902a96997ea5b1542fd0befe5198e3044d1b9c
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-05-13 11:25:11 -0400

    Refactor leap.sh
    Replaced string comparison operators (`==`, `!=`) with numeric comparison operators (`-eq`, `-ne`).

--- bash/leap/leap.sh
+++ bash/leap/leap.sh
@@ -2,13 +2,13 @@
 
 year=$1
 
-if [[ "$#" != 1 ]] || ! [[ $year =~ ^[0-9]{4}$ ]]; then
+if [[ "$#" -ne 1 ]] || ! [[ $year =~ ^[0-9]{4}$ ]]; then
   { echo "Usage: $0 <year>" >&2; exit 1; }
 fi
 
-if [[ $((year % 4)) == 0 ]] \
-  && [[ $((year % 100)) != 0 ]] \
-  || [[ $((year % 400)) == 0 ]]; then
+if [[ $((year % 4)) -eq 0 ]] \
+  && [[ $((year % 100)) -ne 0 ]] \
+  || [[ $((year % 400)) -eq 0 ]]; then
   echo "true"
 else
   echo "false"
