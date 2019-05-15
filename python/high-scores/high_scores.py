de035d08e1f09039f67c7afc99df73124898f622
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-05-15 12:18:18 -0400

    Update high_scores.py
    Refactor after updating to latest version of exercise.

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,28 +1,19 @@
 from heapq import nlargest
 from typing import List
 
-from dataclasses import dataclass
+Scores = List[int]
 
 
-@dataclass
-class HighScores:
-    scores: List[int]
+def latest(scores: Scores) -> int:
+    """The last added score."""
+    return scores[-1]
 
-    def latest(self):
-        """The last added score."""
-        return self.scores[-1]
 
-    def personal_best(self):
-        """The highest score."""
-        return max(self.scores)
+def personal_best(scores: Scores) -> int:
+    """The highest score."""
+    return max(scores)
 
-    def personal_top(self):
-        """The three highest scores."""
-        return nlargest(3, self.scores)
 
-    def report(self):
-        """The difference between the last and the highest scores."""
-        difference = self.personal_best() - self.latest()
-        result_qualifier = (f"{difference} short of " if difference else "")
-        return (f"Your latest score was {self.latest()}. " +
-                f"That's {result_qualifier}your personal best!")
+def personal_top_three(scores: Scores) -> Scores:
+    """The three highest scores."""
+    return nlargest(3, scores)
