94ee07aa0cb52e9716db1c88b0d3071e4fac6ead
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-05-18 15:08:37 -0400

    Solve matrix.rb

---
+++ ruby/matrix/matrix.rb
@@ -0,0 +1,9 @@
+# Return the rows and columns of a string representing a matrix of numbers.
+class Matrix
+  attr_reader :rows, :columns
+
+  def initialize(matrix_string)
+    @rows = matrix_string.lines.map { |line| line.split.map(&:to_i) }
+    @columns = rows.transpose
+  end
+end
