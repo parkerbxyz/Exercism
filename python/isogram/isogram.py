7f0330a57f68add490b78ef4a38df3b17a93db0f
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-06-05 09:28:13 -0400

    Update isogram.py
    Move `.casefold()` from `letter` to `string` for better efficiency.

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,4 +1,4 @@
 def is_isogram(string: str) -> bool:
     """Return True if a given word or phrase is an isogram."""
-    letters = [letter.casefold() for letter in string if letter.isalpha()]
+    letters = [letter for letter in string.casefold() if letter.isalpha()]
     return len(letters) == len(set(letters))
