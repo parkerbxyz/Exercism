d502fd232a27e5eac433b74552b0ed4ae3d2ebdd
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-06-19 12:17:22 -0400

    Remove unnecessary structure

--- swift/hello-world/Sources/HelloWorld/HelloWorld.swift
+++ swift/hello-world/Sources/HelloWorld/HelloWorld.swift
@@ -1,5 +1,3 @@
-struct HelloWorld {
-    static func hello(_ name: String = "World") -> String {
-        return "Hello, \(name)!"
-    }
+func hello(_ name: String = "World") -> String {
+    return "Hello, \(name)!"
 }
