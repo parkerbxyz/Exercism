7021fdf217c6ca80871f0540d9249aa42eb4778f
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-06-27 14:31:31 -0400

    Refactor Leap.swift
    Add new `isMultiple(of:)` method implemented in Swift 5.

--- swift/leap/Sources/Leap/Leap.swift
+++ swift/leap/Sources/Leap/Leap.swift
@@ -1,6 +1,8 @@
 struct Year {
     let calendarYear: Int
     var isLeapYear: Bool {
-        return (calendarYear % 4 == 0) && (calendarYear % 100 != 0) || (calendarYear % 400 == 0)
+        return (calendarYear.isMultiple(of: 4))
+            && (!calendarYear.isMultiple(of: 100))
+            || (calendarYear.isMultiple(of: 400))
     }
 }
