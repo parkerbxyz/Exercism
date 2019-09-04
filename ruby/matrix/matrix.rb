42d6800fd306d386fd8170bea8705a7bc6584009
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-09-04 16:00:43 -0400

    Implement memoization

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,17 +1,14 @@
 # Return the rows and columns of a string representing a matrix of numbers.
 class Matrix
-  attr_reader :rows, :columns
-
   def initialize(matrix_string)
-    @rows = extract_rows(matrix_string)
-    @columns = extract_columns(rows)
+    @matrix = matrix_string
   end
 
-  def extract_rows(matrix_string)
-    matrix_string.lines.map { |line| line.split.map(&:to_i) }
+  def rows
+    @rows ||= @matrix.lines.map { |line| line.split.map(&:to_i) }
   end
 
-  def extract_columns(rows)
-    rows.transpose
+  def columns
+    @columns ||= rows.transpose
   end
 end
