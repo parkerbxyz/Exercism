ca7235bdfd564d658623f27f74c8d78dcfac6f50
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2019-09-30 14:14:13 -0400

    Solve Sum of Multiples

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,12 @@
-def sum_of_multiples(limit, multiples):
-    pass
+from typing import List
+
+
+def sum_of_multiples(limit: int, multiples: List[int]) -> int:
+    """Return the sum of all unique multiples of n less than
+    the 'limit' value for every n in the 'multiples' value.
+    """
+    unique_multiples: set = set()
+    for multiple in (multiple for multiple in multiples if multiple != 0):
+        for number in range(multiple, limit, multiple):
+            unique_multiples.add(number)
+    return sum(unique_multiples)
