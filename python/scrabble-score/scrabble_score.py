9e84ee35ca3f91f39973f07f7695ffd95e4a5a2b
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2020-01-03 17:00:42 -0500

    Refactor scrabble_score.py
    Implement mentor suggestion

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,23 +1,22 @@
 from typing import Dict
 
-LETTER_VALUES: Dict[str, int] = {
-    'A': 1,  'E': 1,
-    'I': 1,  'O': 1,
-    'U': 1,  'L': 1,
-    'N': 1,  'R': 1,
-    'S': 1,  'T': 1,
-    'D': 2,  'G': 2,
-    'B': 3,  'C': 3,
-    'M': 3,  'P': 3,
-    'F': 4,  'H': 4,
-    'V': 4,  'W': 4,
-    'Y': 4,  'K': 5,
-    'J': 8,  'X': 8,
-    'Q': 10, 'Z': 10
-    }
+LETTER_VALUES = (
+    ('A, E, I, O, U, L, N, R, S, T',    1),
+    ('D, G',                            2),
+    ('B, C, M, P',                      3),
+    ('F, H, V, W, Y',                   4),
+    ('K',                               5),
+    ('J, X',                            8),
+    ('Q, Z',                            10)
+)
+
+POINTS: Dict[str, int] = {
+    letter: value for letters, value in LETTER_VALUES
+    for letter in letters.split(', ')
+}
 
 
 def score(word: str) -> int:
     """Return the Scrabble score for the word."""
     word = word.upper()
-    return sum(LETTER_VALUES.get(letter, 0) for letter in word)
+    return sum(POINTS.get(letter, 0) for letter in word)
