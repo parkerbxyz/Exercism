f053cfd03894dbb1229c9134fea08b5bcaa2da19
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2022-09-03 21:43:02 -0700

    Remove boilerplate comments

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -1,24 +1,17 @@
 #!/usr/bin/env bash
+#
+# Converts a long name like Portable Network Graphics to its acronym (PNG).
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+# Set input as variable
+input=$1
+
+# Set space as delimiter
+IFS=' '
+
+# Read the split words into an array based on space delimiter
+read -ra acronym <<< "$input"
+
+# Print the acronym
+for word in "${acronym[@]}"; do
+  echo -n "${word:0:1}"
+done
